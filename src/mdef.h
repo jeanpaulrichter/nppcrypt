1ec2f1d5f6ed3df77ec854654885316b9bbc33d0
jeanpaulrichter <l.schindler@cerberus-design.de>
2017-07-10 15:00:25 +0200

    increased version number to 1.0.1.4

--- src/mdef.h
+++ src/mdef.h
@@ -16,14 +16,14 @@ GNU General Public License for more details.
 #define MDEF_DEFINE_H
 
 #define		NPPC_NAME					"NppCrypt"
-#define		NPPC_VERSION				1013
+#define		NPPC_VERSION				1014
 
 #define		NPPC_FUNC_COUNT				9
 #define		NPPC_FUNC_HASH_ID			2
 #define		NPPC_FUNC_RANDOM_ID			3
 #define		NPPC_FUNC_CONVERT_ID		4
 
-#define		NPPC_ABOUT_TEXT				"nppcrypt v1.0.1.3"
+#define		NPPC_ABOUT_TEXT				"nppcrypt v1.0.1.4"
 #define		NPPC_ABOUT_LINK				"www.cerberus-design.de/nppcrypt/readme"
 #define		NPPC_ABOUT_URL				"http://www.cerberus-design.de/nppcrypt/readme.txt"
 
